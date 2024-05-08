58a8c48f164fdf75a14bb4094c27b377d2e2a356
MJLuke <lmj3326679252@gmail.com>
2024-05-08 06:15:24 -0400

    save

--- QuickSelect1.cpp
+++ QuickSelect1.cpp
@@ -14,37 +14,34 @@ int MyPartition(std::vector<int>& data, int low, int high)//low is beging  ,high
     return i;
 } 
 int kthSmallest(std::vector<int>& data,int low, int high, int k){
-    if(k <= high - low + 1&&k>0){
-        if(low==high){
-            return data[low];
-        }   
-        int p = MyPartition(data,low,high);
-        int length = p - low + 1;
-        if(k == length){
-            return data[p];
-        }
-        else if(k<length){
-            return kthSmallest(data, low, p-1,k);
-        }
-        else {
-            return kthSmallest(data, p+1, high, k- length);
-        }
+    if (low == high) {
+        return data[low];
     }
-    return INT_MAX;
+    int pi = MyPartition(data, low, high);
+    if (k== pi) {
+        return data[pi];
+    } 
+    if (k < pi) {
+        return kthSmallest(data, low, pi-1, k);
+    } 
+    else {
+        return kthSmallest(data, pi+1, high, k);
+    }
 }
 
 void quickSelect1 (const std::string & header, std::vector<int>& data){
     std::cout<<header<<std::endl;
 
     int size=data.size();
-    int median=kthSmallest(data,0,size-1,size/2);
-    int P25=kthSmallest(data,0,size-1,size/4);
-    int P75= kthSmallest(data,0,size-1,3*size/4);
+    int median=kthSmallest(data,0,size-1,(size/2)-1);
+    //std::cout<<"pi :"<<(size/2)-1<<std::endl;
+    //std::cout<<"1/4:"<<(size/4)-1<<std::endl;
+    int P25= kthSmallest(data,0,size-1,(size/4)-1);
+    int P75= kthSmallest(data,0,size-1,(3*size/4)-1);
  
     std::cout << "Min: " << *std::min_element(data.begin(), data.end()) << std::endl;
     std::cout << "P25: " << P25 << std::endl;
     std::cout << "P50: " << median << std::endl;
     std::cout << "P75: " << P75 << std::endl;
     std::cout << "Max: " << *std::max_element(data.begin(), data.end()) << std::endl;
-
 }
